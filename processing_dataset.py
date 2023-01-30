81946bb75646f4208581d01123ad0e672f89db09
ralfcat <victor.englof@hotmail.com>
2023-01-30 16:21:14 +0100

    modded

--- processing_dataset.py
+++ processing_dataset.py
@@ -7,4 +7,4 @@ df = pd.read_csv("processed_blackjack_dataset.csv")
 df = df[df["win"] >= 0]
 
 # Save the updated DataFrame to a new file or overwrite the original file
-df.to_csv("processed_blackjack_dataset", index=False)
+df.to_csv("processed_blackjack_dataset.csv", index=False)
