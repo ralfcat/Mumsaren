9659a927a9001abfba3671c9450629160fd1e8e4
ralfcat <victor.englof@hotmail.com>
2023-01-31 23:46:31 +0100

    pol

--- processing_dataset.py
+++ processing_dataset.py
@@ -1,6 +1,14 @@
+import ast
 import pandas as pd
-import numpy as np
 
-df = pd.read_csv("dataset_true.csv")
-df = df[["dealer_up", "initial_hand","actions_taken"]]
-df.to_csv("dataset_true.csv", index=False)
+# Load the CSV file into a pandas DataFrame
+df = pd.read_csv('dataset_true.csv')
+
+# Define a function to convert the strings to lists and remove the double quotes
+def convert_to_list(string):
+    string = string.replace('"', '')
+    return ast.literal_eval(string)
+
+# Apply the function to the column of interest
+df['initial_hand'] = df['initial_hand'].apply(convert_to_list)
+df.to_csv('dataset_true2.csv', index=False)
