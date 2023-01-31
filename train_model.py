08424cbd99f38fb32599fb266ffce365e048663d
Olle Rahm <ollerahm50@gmail.com>
2023-01-31 23:42:56 +0100

    a

--- train_model.py
+++ train_model.py
@@ -4,17 +4,30 @@ from keras.layers import Input
 import numpy as np
 import pandas as pd
 
-dataset = pd.read_csv("processed_blackjack_dataset.csv").to_numpy()
+def strToList(str):
+    """
+    Input is a list of string type with integers, ex: "[1, 2, 3]"
+    Output is said list as a list
+    """
+    templist = str[1:-1].replace(",", "").split()
+    finallist = np.zeros(len(templist))
+    for i, card in enumerate(templist):
+        finallist[i] = int(card)
+    return finallist
+
+dataset = pd.read_csv("processed_blackjack_dataset.csv").to_numpy()[:10, :]
 
 Dealer = dataset[:, 0]
 Hand = dataset[:, 1]
 Y = dataset[:2, 6:7]
 
-X = np.zeros(dataset.shape[0], )
+X = np.zeros((dataset.shape[0], 6))
+X [:, 0] = Dealer
 for i in range(dataset.shape[0]):
+    Hand_current = strToList(Hand[i])
+    print(Hand_current)
 
-
-# X = np.array([[dealer, c1, c2, c3, c4, c5]])
+# X = np.array([[dealer, card1, card2, card3, card4, card5]])
 # Y = np.array([[action]])
 
 print(Y)
@@ -32,11 +45,3 @@ model.add(Dense(5, activation='softmax'))
 # model.fit(X_train, y_train, epochs=10, batch_size=32)
 
 # model.save("trained_model")
-
-card1_array = np.zeros(Xtest)
-for hand in X:
-
-Xtest = X[:2, 1]
-print(Xtest)
-Xtest = Xtest.replace(",", "")
-print(Xtest[1:-1].split())
