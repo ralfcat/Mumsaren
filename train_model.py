932c18e7aadd390f3a6af174b94016b728f04ea0
Olle Rahm <ollerahm50@gmail.com>
2023-02-01 01:32:57 +0100

    Kinda funkar

--- train_model.py
+++ train_model.py
@@ -34,18 +34,36 @@ def YToArray(elem):
         return np.array([[0, 0, 0, 1, 0, 0]])
     if letter == "N":
         return np.array([[0, 0, 0, 0, 1, 0]])
-    
 
+
+def ArrayToY(elem):
+    """
+    Converts the output of the network to the desired action.
+    """
+    small = 1
+    if np.linalg.norm(elem - np.array([[1, 0, 0, 0, 0, 0]])) <= small:
+        return "Stand"
+    elif np.linalg.norm(elem - np.array([[0, 1, 0, 0, 0, 0]])) <= small:
+        return "Hit"
+    elif np.linalg.norm(elem - np.array([[0, 0, 1, 0, 0, 0]])) <= small:
+        return "Double Down"
+    elif np.linalg.norm(elem - np.array([[0, 0, 0, 1, 0, 0]])) <= small:
+        return "Split"
+    elif np.linalg.norm(elem - np.array([[0, 0, 0, 0, 1, 0]])) <= small:
+        return "No Insurance"
+    elif np.linalg.norm(elem - np.array([[0, 0, 0, 0, 0, 1]])) <= small:
+        return "Black Jack"
+    
 
 dataset = pd.read_csv("processed_blackjack_dataset.csv").to_numpy()
 
-Dealer = dataset[:, 0]
-Hand = dataset[:, 1]
-Event = dataset[:, 6:7]
+Dealer = dataset[:, 0]      #Shape: (n_datapoints, ) type: int 
+Hand = dataset[:, 1]        #Shape: (n_datapoints, ) type: string of the form '[2, 3]'
+Event = dataset[:, 6]       #Shape: (n_datapoints, ) type: string of the form '[['P', 'H'], ['H', 'S']]'
 
-#print(Event[0])
-#print(YToArray(Event[0]))
-
+# Create X and Y such that:
+# X = np.array([[dealer, card1, card2, card3, card4, card5]])
+# Y = np.array([[action]])
 X = np.zeros((dataset.shape[0], 6))
 X [:, 0] = Dealer
 for i in range(dataset.shape[0]):
@@ -56,16 +74,16 @@ Y = np.zeros((Event.shape[0], 6))
 for i in range(Event.shape[0]):
     Y[i] = YToArray(Event[i])
 
-# X = np.array([[dealer, card1, card2, card3, card4, card5]])
-# Y = np.array([[action]])
 
-X_train = X[1000:, :]
-Y_train = Y[1000:, :]
+
+X_train = X
+Y_train = Y
 
 # define the architecture of the model
 model = Sequential()
-model.add(Dense(16, input_dim=6, activation='relu'))
-model.add(Dense(16, activation='relu'))
+model.add(Dense(32, input_dim=6, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(6, activation='softmax'))
 
 # compile the model
@@ -83,6 +101,5 @@ while True:
         ls.append(0)
     ls = [int(elem) for elem in ls]
     arr = np.array([ls])
-    print(arr)
-    print(model(arr))
+    print(ArrayToY(model(arr)))
 
